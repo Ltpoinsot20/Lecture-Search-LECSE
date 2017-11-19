7c07f0092cf599c463ef47aa235ba07358006df9
Ltpoinsot20 <33100822+Ltpoinsot20@users.noreply.github.com>
2017-11-19 17:03:28 -0500

    text_writer update

--- LECSE/src/LECSE/text_writer.java
+++ LECSE/src/LECSE/text_writer.java
@@ -15,33 +15,33 @@ public class text_writer{
 
 	public static void main(String[] args) throws Exception, UnsupportedAudioFileException {
 
-		AudioPlayer player = new AudioPlayer(soundFile);
-		player.run();
+//		AudioPlayer player = new AudioPlayer(soundFile);
+//		player.run();
 		
-		//		BufferedWriter bw = null;
-		//		FileWriter fw = null;
-		//		
-		//		wavREC.startRecognition();
-		//		String [] confidence = wavREC.getConfidence();
-		//		String content = wavREC.getOutput();
-		//
-		//		try {
-		//			fw = new FileWriter(FILENAME);
-		//			bw = new BufferedWriter(fw);
-		//			bw.write(content);
-		//		} catch (IOException e) {
-		//			e.printStackTrace();
-		//		} 
-		//
-		//		finally {
-		//			try {
-		//				if (bw != null)
-		//					bw.close();
-		//				if (fw != null)
-		//					fw.close();
-		//			} catch (IOException ex) {
-		//				ex.printStackTrace();
-		//			}
-		//		}
+				BufferedWriter bw = null;
+				FileWriter fw = null;
+				
+				wavREC.startRecognition();
+				String [] confidence = wavREC.getConfidence();
+				String content = wavREC.getOutput();
+		
+				try {
+					fw = new FileWriter(FILENAME);
+					bw = new BufferedWriter(fw);
+					bw.write(content);
+				} catch (IOException e) {
+					e.printStackTrace();
+				} 
+		
+				finally {
+					try {
+						if (bw != null)
+							bw.close();
+						if (fw != null)
+							fw.close();
+					} catch (IOException ex) {
+						ex.printStackTrace();
+					}
+				}
 	}
 }
