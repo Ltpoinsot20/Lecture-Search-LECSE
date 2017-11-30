6e11957ce303ded553b0f83ba8acbf7c18754e85
Ltpoinsot20 <33100822+Ltpoinsot20@users.noreply.github.com>
2017-11-30 18:44:22 -0500

    text_writer update
    convert text_writer to object class

--- LECSE/src/LECSE/text_writer.java
+++ LECSE/src/LECSE/text_writer.java
@@ -5,28 +5,38 @@ import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 
-import javax.sound.sampled.UnsupportedAudioFileException;
+//import javax.sound.sampled.UnsupportedAudioFileException;
+//		AudioPlayer player = new AudioPlayer(soundFile);
+//		player.run();
 
 public class text_writer{
 
-	private static final String FILENAME = "output.txt";
-	private static File soundFile = new File("Nimarcus.wav");
-	private static wav_recognizer wavREC = new wav_recognizer(soundFile);
+	private String filename;
+	private File soundFile;
+	private String filepath;
+	private wav_recognizer wavREC = new wav_recognizer(soundFile);
 
-	public static void main(String[] args) throws Exception, UnsupportedAudioFileException {
-
-		//		AudioPlayer player = new AudioPlayer(soundFile);
-		//		player.run();
-
+	public text_writer(String filename, File soundFile, String file_path){
+		setfilename(filename);
+		setsoundFile(soundFile);
+		setfilepath(file_path);
+	}
+	
+	public String get_recognized_text() {
+		wavREC.startRecognition();
+		//String [] confidence = wavREC.getConfidence();
+		String content = wavREC.getOutput();
+		return content;
+	}
+	
+	public void file_writer(String content) {
 		BufferedWriter bw = null;
 		FileWriter fw = null;
-
-		wavREC.startRecognition();
-		String [] confidence = wavREC.getConfidence();
-		String content = wavREC.getOutput();
+		filename = filepath + File.separator + filename;
+		
 
 		try {
-			fw = new FileWriter(FILENAME);
+			fw = new FileWriter(filename);
 			bw = new BufferedWriter(fw);
 			bw.write(content);
 		} catch (IOException e) {
@@ -43,5 +53,17 @@ public class text_writer{
 				ex.printStackTrace();
 			}
 		}
+	}
+	
+	public void setfilename(String filename){
+		this.filename = filename;
+	}
+	
+	public void setsoundFile(File soundFile){
+		this.soundFile = soundFile;
+	}
+	
+	public void setfilepath(String filepath){
+		this.filepath = filepath;
 	}
 }
